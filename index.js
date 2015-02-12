a27733f1f65d5d4adf8c3ef0a87e8997bc213f65
Jorge Rubio <jorgerc85@gmail.com>
2015-02-12 16:08:24 -0500

    Index.js: Add hideSections function to hide unused sections

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function sectionShow() {
   var sectionControls = {'home_btn': 'home', 'about_btn': 'about', 'projects_btn': 'projects', 'blog_btn': 'blog'}
   document.getElementById('menu').addEventListener('click', function(event) {
     event.preventDefault();
+    hideSections(sectionControls);
     document.getElementById(sectionControls[event.target.id]).style.display = 'inline';
   });
+};
+
+function hideSections(sections) {
+  for (i in sections) {
+    document.getElementById(sections[i]).style.display = 'none';
+  };
 };
