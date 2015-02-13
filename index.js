874a0cbcec38af538cfab81e863bf3365f48c1af
Jorge Rubio <jorgerc85@gmail.com>
2015-02-12 19:32:52 -0500

    Index.js: Add logic to load blog posts in sequence

--- index.js
+++ index.js
@@ -19,8 +19,18 @@ function hideSections(sections) {
 };
 
 function blogController() {
+  var i = 0;
+  var blogList = ['blog/t8-tech.html', 'blog/t7-JavaScript.html', 'blog/t6-oop-concepts.html', 'blog/t5-ruby-classes.html',
+                  'blog/t4-enumerable-methods.html', 'blog/t3-arrays-hashes.html', 'blog/t2-css-design.html',
+                  'blog/t1-git-blog.html'];
+  ajaxWrapper(blogList[i])
   document.getElementById('blog_control').addEventListener('click', function(event) {
     event.preventDefault();
+    if (event.target.id == 'show_older_btn') {
+      i += 1;
+      ajaxWrapper(blogList[i]);
+    } else if (event.target.id == 'show_newer_btn') {
+      i -= 1;
       ajaxWrapper(blogList[i]);
     };
   });
